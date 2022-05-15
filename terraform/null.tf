22749beab5781a02ccf42bce51ac1fe6c181ad73
AbdEl-RahmanKhaled <a.kha.anw@gmail.com>
2022-05-15 19:37:56 +0200

    update Jenkinsfile

--- terraform/null.tf
+++ terraform/null.tf
@@ -5,17 +5,27 @@ resource "null_resource" "out" {
   }
 
   provisioner "local-exec" {
+    command = "echo 'StrictHostKeyChecking=no' >> ../ansible/inventory"
+ 
+  }
+
+  provisioner "local-exec" {
+    command = "echo 'ServerAliveInterval=5' >> ../ansible/inventory"
+ 
+  }
+  provisioner "local-exec" {
     command = "echo 'ansible_ssh_common_args=\"-o ProxyCommand=\\\"ssh -i ../key/key.pem -t -W %h:%p -q ubuntu@${aws_instance.pub-ec2.public_dns}\\\"\" \n\n[private]' >> ../ansible/inventory"
  
   }
 
+ 
   provisioner "local-exec" {
     command = "echo '${aws_instance.priv-ec2.private_ip}' >> ../ansible/inventory && echo '${tls_private_key.private_key_pair.private_key_pem}' > ../key/key.pem && chmod 600 ../key/key.pem"
  
   }
 
   provisioner "local-exec" {
-    command = "echo '${tls_private_key.private_key_pair.private_key_pem}' > ../key/key.pem && chmod 600 ../key/key.pem"
+    command = "echo '${tls_private_key.private_key_pair.private_key_pem}' > /home/$USER/.ssh/key.pem && chmod 600 /home/$USER/.ssh/key.pem"
  
   }
 }
