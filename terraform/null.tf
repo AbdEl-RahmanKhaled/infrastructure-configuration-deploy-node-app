c194129cd9ee39519ecad3b0320519a411dc4f24
AbdEl-RahmanKhaled <a.kha.anw@gmail.com>
2022-05-17 08:59:12 +0200

    update jenkinsfile

--- terraform/null.tf
+++ terraform/null.tf
@@ -1,8 +1,7 @@
 resource "null_resource" "out" {
   provisioner "local-exec" {
     command = <<EOF
-      echo '${tls_private_key.private_key_pair.private_key_pem}' > /var/jenkins_home/.ssh/key.pem && chmod 600  /var/jenkins_home/.ssh/key.pem
-      echo '${tls_private_key.private_key_pair.private_key_pem}' > /root/.ssh/key.pem && chmod 600  /root/.ssh/key.pem
+      echo '${tls_private_key.private_key_pair.private_key_pem}' > $HOME/.ssh/key.pem && chmod 600  /root/.ssh/key.pem
     EOF
   }
   
