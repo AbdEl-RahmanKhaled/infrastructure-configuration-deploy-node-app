0a5001e228888fc4ea2a93bc1f834dc422ac7011
AbdEl-RahmanKhaled <a.kha.anw@gmail.com>
2022-05-19 19:42:16 +0200

    update script

--- scripts/config-file.sh
+++ scripts/config-file.sh
@@ -4,10 +4,12 @@ host bastion
    HostName $1
    User ubuntu
    identityFile $HOME/.ssh/key.pem
+   StrictHostKeyChecking=no
 
 host private_instance
    HostName  $2
    user  ubuntu
    ProxyCommand ssh bastion -W %h:%p
    identityFile $HOME/.ssh/key.pem
+   StrictHostKeyChecking=no
 EOF
