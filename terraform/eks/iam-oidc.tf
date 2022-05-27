02c9bbfb7e3e570461803d27e369f596b8f76dea
AbdEl-RahmanKhaled <a.kha.anw@gmail.com>
2022-05-27 19:42:18 +0200

    update eks conf

---
+++ terraform/eks/iam-oidc.tf
@@ -0,0 +1,32 @@
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.aws_eks.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.aws_eks.identity[0].oidc[0].issuer
+}
+
+data "aws_iam_policy_document" "oidc_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "oidc_role" {
+  assume_role_policy = data.aws_iam_policy_document.oidc_assume_role_policy.json
+  name               = "oidc-role"
+}
