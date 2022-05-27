5557a01fb837422a54cc7ec301e4e801cebac02f
AbdEl-RahmanKhaled <a.kha.anw@gmail.com>
2022-05-27 16:47:37 +0200

    fix eks

--- terraform/eks/eks.tf
+++ terraform/eks/eks.tf
@@ -1,10 +1,10 @@
 resource "aws_eks_cluster" "aws_eks" {
   name     = "${var.ws_name}_eks_cluster"
   role_arn = aws_iam_role.eks_cluster.arn
-  endpoint_private_access = true
-
+  
   vpc_config {
     subnet_ids = [var.eks_sub_1, var.eks_sub_2]
+    endpoint_private_access = true
   }
 
   tags = {
